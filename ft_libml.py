4076b09a882f21980e407de0bff4f0ca0d5abb9a
Leo SIMON <lsimon@e1r8p6.42.fr>
2018-12-15 12:07:39 +0100

    libml: add normalize function

--- ft_libml.py
+++ ft_libml.py
@@ -1,5 +1,14 @@
 import math
 import pandas as pd
+from sklearn import preprocessing
+from load_datas import courses
+
+#TODO: remove sklearn
+def normalize(df):
+	x = df.values #returns a numpy array
+	min_max_scaler = preprocessing.MinMaxScaler()
+	x_scaled = min_max_scaler.fit_transform(x)
+	return pd.DataFrame(x_scaled, columns=courses)
 
 def count(x):
     d = x.dropna()
